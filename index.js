4e452de5d9a1b530ca473bb01bce9f6a6653c15d
Palak-137 <kumaripalak.aps@gmail.com>
2021-06-22 10:10:06 +0530

    commeted the code and made a block file for output

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
+
+//importing the library to open , write , read the files.
 const fs = require('fs');
 const csv = require('csvtojson');
 const {Parser} = require('json2csv');
 
 (async()=>{
     const mempool = await csv().fromFile("mempool.csv");
-
     console.log(mempool);
 })();
+
+// an JS object to store all the valid transctions
+let validTransction = {}
+
+// 
+
+fs.writeFile('block.txt',validTransction,(err)=>{
+    if(err){
+        throw err;
+    }
+})
